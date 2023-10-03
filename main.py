66f2399839afd1eb357810fcdf79cc2f0e790c7d
User mame <gfhrjdfz5@bk.ru>
2023-10-03 10:15:10 +0300

    add py file

--- main.py
+++ main.py
@@ -19,12 +19,11 @@ if not table_exists:
                       (link TEXT, color TEXT, price INTEGER)''')
     conn.commit()
 
-def format_print(name_f):
+def format_print(name_f): #возвращает строку для вывода, а так же добавляет данные в бд
     df = pd.read_excel(f"{name_f}")
-    # Вывести таблицу в формате xlsx
+
     conn = sqlite3.connect('parsing.db')
     cursor = conn.cursor()
-
     strok = "Сайт:                        |Цвет:               |Цена: \n"
     # 14 18 3
     for index, row in df.iterrows():
@@ -37,13 +36,10 @@ def format_print(name_f):
         existing_data = cursor.fetchall()
         if not existing_data:
             cursor.execute('INSERT INTO data_cup (link, color, price) VALUES (?, ?, ?)', (link, color, price))
-
         strok += (' |'.join(row_values)) + "\n"
         # strok += f'{link} {" "*(14-len(link))} {color} {price} \n'
-
     conn.commit()
     conn.close()
-
     return strok
 
 def print_max(name_f):
@@ -52,8 +48,8 @@ def print_max(name_f):
     avg_prices = df.groupby('Сайт')['Цена'].mean().reset_index()
     for index, row in avg_prices.iterrows():
         a += f"{row['Сайт']} составляет: {int(row['Цена'])} \n"
-
     return a
+
 @bot.message_handler(commands=['start'])
 def start(message):
     button = KeyboardButton('Загрузить файл 📎')
@@ -61,34 +57,28 @@ def start(message):
     markup.add(button)
     bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}! \nНажми на кнопку "Загрузить файл 📎".', reply_markup=markup)
 
-
 @bot.message_handler(func=lambda message: message.text == 'Загрузить файл 📎')
 def attach_file(message):
     bot.send_message(message.chat.id, 'Чтобы прикрепить файл, нажмите на 📎 -> Файл, и выберите необходимый.')
 
-# Функция для обработки сообщения с документом
 @bot.message_handler(regexp=r'https?://[^\s]+')
 def handle_link(message):
     url = message.text
     response = requests.get(url)
-
     with open('file2.xlsx', 'wb') as file:
         file.write(response.content)
     name_ff = 'file2.xlsx'
-
     try:
         bot.send_message(message.chat.id, format_print(name_ff))
         bot.send_message(message.chat.id, print_max(name_ff))
     except Exception as e:
         bot.reply_to(message, "Похоже ваш файл не удовлетворяет условию")
         os.remove(f"{name_ff}")
+
 @bot.message_handler(content_types=['document'])
 def handle_document(message):
-    # Получаем информацию о файле
     file_info = bot.get_file(message.document.file_id)
-    # Скачиваем файл
     downloaded_file = bot.download_file(file_info.file_path)
-    # Сохраняем файл на диск
     with open(message.document.file_name, 'wb') as file:
         file.write(downloaded_file)
     name_file = message.document.file_name
